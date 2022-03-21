69a8829d662e8c5f973e8ceacd199408e3ead6d3
leandrocgsi <leandrocgsi@gmail.com>
2022-03-21 20:42:59 -0300

    Working with four HTTP verbs

--- 07_WorkingWithVerbs/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/controller/PersonController.kt
+++ 07_WorkingWithVerbs/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/controller/PersonController.kt
@@ -3,69 +3,58 @@ package br.com.erudio.controller
 import br.com.erudio.converters.NumberConverter
 import br.com.erudio.exceptions.UnsupportedMathOperationException
 import br.com.erudio.math.SimpleMath
+import br.com.erudio.model.Person
+import br.com.erudio.services.PersonService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
 
 @RestController
-class MathController {
+@RequestMapping("/person")
+class PersonController {
 
-    val counter: AtomicLong = AtomicLong()
 
-    private val math: SimpleMath = SimpleMath()
+    @Autowired
+    private lateinit var service: PersonService
+    // var service: PersonService = PersonService()
 
-    @RequestMapping(value = ["/sum/{numberOne}/{numberTwo}"])
-    fun sum(@PathVariable(value="numberOne") numberOne: String?,
-            @PathVariable(value="numberTwo") numberTwo: String?
-    ): Double {
-        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
-            throw UnsupportedMathOperationException("Please set a numeric value!")
-        return math.sum(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo))
+    @RequestMapping(method = [RequestMethod.GET],
+        produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun findAll(): List<Person> {
+        return service.findAll()
     }
 
-    @RequestMapping(value = ["/subtraction/{numberOne}/{numberTwo}"])
-    fun subtraction(@PathVariable(value="numberOne") numberOne: String?,
-                    @PathVariable(value="numberTwo") numberTwo: String?
-    ): Double {
-        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
-            throw UnsupportedMathOperationException("Please set a numeric value!")
-        return math.subtraction(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo))
+    @RequestMapping(value = ["/{id}"], method = [RequestMethod.GET],
+                    produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun findById(@PathVariable(value="id") id: Long): Person {
+        return service.findById(id)
     }
 
-    @RequestMapping(value = ["/multiplication/{numberOne}/{numberTwo}"])
-    fun multiplication(@PathVariable(value="numberOne") numberOne: String?,
-                       @PathVariable(value="numberTwo") numberTwo: String?
-    ): Double {
-        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
-            throw UnsupportedMathOperationException("Please set a numeric value!")
-        return math.multiplication(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo))
+    @RequestMapping(method = [RequestMethod.POST],
+                    consumes = [MediaType.APPLICATION_JSON_VALUE],
+                    produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun create(@RequestBody person: Person): Person {
+        return service.create(person)
     }
 
-    @RequestMapping(value = ["/division/{numberOne}/{numberTwo}"])
-    fun division(@PathVariable(value="numberOne") numberOne: String?,
-                 @PathVariable(value="numberTwo") numberTwo: String?
-    ): Double {
-        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
-            throw UnsupportedMathOperationException("Please set a numeric value!")
-        return math.division(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo))
+    @RequestMapping(method = [RequestMethod.PUT],
+                    consumes = [MediaType.APPLICATION_JSON_VALUE],
+                    produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun update(@RequestBody person: Person): Person {
+        return service.update(person)
     }
 
-    @RequestMapping(value = ["/mean/{numberOne}/{numberTwo}"])
-    fun mean(@PathVariable(value="numberOne") numberOne: String?,
-             @PathVariable(value="numberTwo") numberTwo: String?
-    ): Double {
-        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
-            throw UnsupportedMathOperationException("Please set a numeric value!")
-        return math.mean(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo))
+    @RequestMapping(value = ["/{id}"], method = [RequestMethod.DELETE],
+        produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun delete(@PathVariable(value="id") id: Long) {
+        service.delete(id)
     }
 
-    @RequestMapping(value = ["/squareRoot/{number}"])
-    fun squareRoot(@PathVariable(value="number") number: String?
-    ): Double {
-        if (!NumberConverter.isNumeric(number))
-            throw UnsupportedMathOperationException("Please set a numeric value!")
-        return math.squareRoot(NumberConverter.convertToDouble(number))
-    }
+
 
 }
