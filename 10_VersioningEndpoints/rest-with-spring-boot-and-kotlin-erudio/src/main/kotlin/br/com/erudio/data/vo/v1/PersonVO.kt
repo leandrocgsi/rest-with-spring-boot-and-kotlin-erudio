7703eb784118e706b733e0fcce01bff4be57a87f
leandrocgsi <leandrocgsi@gmail.com>
2022-03-22 19:13:29 -0300

    Versioning our API

---
+++ 10_VersioningEndpoints/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/data/vo/v1/PersonVO.kt
@@ -0,0 +1,10 @@
+package br.com.erudio.data.vo.v1
+
+data class PersonVO (
+
+    var id: Long = 0,
+    var firstName: String = "",
+    var lastName: String = "",
+    var address: String = "",
+    var gender: String = ""
+)
