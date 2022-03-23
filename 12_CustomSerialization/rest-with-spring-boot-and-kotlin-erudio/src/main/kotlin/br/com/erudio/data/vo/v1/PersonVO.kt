4c467558c8c81b07b39c12e2831c19ebdd73233a
leandrocgsi <leandrocgsi@gmail.com>
2022-03-23 17:31:26 -0300

    Custom JSON serialization

--- 12_CustomSerialization/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/data/vo/v1/PersonVO.kt
+++ 12_CustomSerialization/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/data/vo/v1/PersonVO.kt
@@ -1,10 +1,21 @@
 package br.com.erudio.data.vo.v1
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+
+@JsonPropertyOrder("id", "address", "first_name", "last_name", "gender")
 data class PersonVO (
 
     var id: Long = 0,
+
+    @field:JsonProperty("first_name")
     var firstName: String = "",
+
+    @field:JsonProperty("last_name")
     var lastName: String = "",
     var address: String = "",
+
+    @field:JsonIgnore
     var gender: String = ""
 )
