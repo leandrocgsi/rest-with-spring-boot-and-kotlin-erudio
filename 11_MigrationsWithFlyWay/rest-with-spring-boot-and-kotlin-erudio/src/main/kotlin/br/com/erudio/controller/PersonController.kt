a32336ba4561a4d0b01ef4012e7df771a15f9611
leandrocgsi <leandrocgsi@gmail.com>
2022-03-23 16:30:58 -0300

    Adding migrations support with Flyway

--- 11_MigrationsWithFlyWay/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/controller/PersonController.kt
+++ 11_MigrationsWithFlyWay/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/controller/PersonController.kt
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 class PersonController {
 
 
