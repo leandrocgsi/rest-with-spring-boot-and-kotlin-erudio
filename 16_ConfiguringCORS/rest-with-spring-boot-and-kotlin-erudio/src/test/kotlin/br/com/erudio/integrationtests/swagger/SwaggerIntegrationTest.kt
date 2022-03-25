f2125c59c8cc2714d8da0da45fefbafbf8d940fa
leandrocgsi <leandrocgsi@gmail.com>
2022-03-25 15:10:57 -0300

    Configuring Cross-Origin Resource Sharing (CORS)

--- 16_ConfiguringCORS/rest-with-spring-boot-and-kotlin-erudio/src/test/kotlin/br/com/erudio/integrationtests/swagger/SwaggerIntegrationTest.kt
+++ 16_ConfiguringCORS/rest-with-spring-boot-and-kotlin-erudio/src/test/kotlin/br/com/erudio/integrationtests/swagger/SwaggerIntegrationTest.kt
@@ -1,6 +1,6 @@
 package br.com.erudio.integrationtests.swagger
 
-import br.com.erudio.integrationtests.ConfigsTest
+import br.com.erudio.integrationtests.TestConfigs
 import br.com.erudio.integrationtests.testcontainers.AbstractIntegrationTest
 import io.restassured.RestAssured.given
 import org.junit.jupiter.api.Assertions.assertTrue
@@ -14,7 +14,7 @@ class SwaggerIntegrationTest() : AbstractIntegrationTest() {
 	fun shouldDisplaySwaggerUiPage() {
 		val content = given()
 			.basePath("/swagger-ui/index.html")
-			.port(ConfigsTest.SERVER_PORT)
+			.port(TestConfigs.SERVER_PORT)
 				.`when`()
 			.get()
 			.then()
