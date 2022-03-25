7f3ab586e1c12a1caa9ef98f74b282148f701a75
leandrocgsi <leandrocgsi@gmail.com>
2022-03-25 15:11:13 -0300

    Configuring Cross-Origin Resource Sharing (CORS)

---
+++ 16_ConfiguringCORS/rest-with-spring-boot-and-kotlin-erudio/src/test/kotlin/br/com/erudio/integrationtests/vo/PersonVO.kt
@@ -0,0 +1,10 @@
+package br.com.erudio.integrationtests.vo
+
+data class PersonVO (
+
+    var id: Long = 0,
+    var firstName: String = "",
+    var lastName: String = "",
+    var address: String = "",
+    var gender: String = ""
+)
