7b6a756f154e8690abdd2ff842ba2423380f03cb
leandrocgsi <leandrocgsi@gmail.com>
2022-03-28 10:27:16 -0300

    Shielding our API with Tests

---
+++ 18_ShieldingOurAPIWithTests/rest-with-spring-boot-and-kotlin-erudio/src/test/kotlin/br/com/erudio/integrationtests/controller/withjson/PersonControllerJsonTest.kt
@@ -0,0 +1,106 @@
+package br.com.erudio.integrationtests.controller.withjson
+
+import br.com.erudio.integrationtests.TestConfigs
+import br.com.erudio.integrationtests.testcontainers.AbstractIntegrationTest
+import br.com.erudio.integrationtests.vo.AccountCredentialsVO
+import br.com.erudio.integrationtests.vo.PersonVO
+import br.com.erudio.integrationtests.vo.TokenVO
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.restassured.RestAssured
+import io.restassured.RestAssured.given
+import io.restassured.builder.RequestSpecBuilder
+import io.restassured.filter.log.LogDetail
+import io.restassured.filter.log.RequestLoggingFilter
+import io.restassured.filter.log.ResponseLoggingFilter
+import io.restassured.specification.RequestSpecification
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.*
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class PersonControllerJsonTest : AbstractIntegrationTest() {
+
+    private lateinit var specification: RequestSpecification
+    private lateinit var objectMapper: ObjectMapper
+    private lateinit var person: PersonVO
+
+    @BeforeAll
+    fun setupTests(){
+        objectMapper = ObjectMapper()
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+        person = PersonVO()
+    }
+
+    @Test
+    @Order(0)
+    fun testLogin() {
+        val user = AccountCredentialsVO(
+            username = "leandro",
+            password = "admin123"
+        )
+
+        val token = RestAssured.given()
+            .basePath("/auth/signin")
+                .port(TestConfigs.SERVER_PORT)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .body(user)
+            .`when`()
+            .post()
+                .then()
+                    .statusCode(200)
+                        .extract()
+                    .body()
+                        .`as`(TokenVO::class.java)
+                            .accessToken
+
+        specification = RequestSpecBuilder()
+            .addHeader(TestConfigs.HEADER_PARAM_AUTHORIZATION, "Bearer $token")
+                .setBasePath("/api/person/v1")
+            .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(ResponseLoggingFilter(LogDetail.ALL))
+            .build()
+    }
+
+    @Test
+    @Order(1)
+    fun testCreate() {
+        mockPerson()
+
+        val content = given()
+            .spec(specification)
+            .contentType(TestConfigs.CONTENT_TYPE_JSON)
+            .body(person)
+            .`when`()
+            .post()
+            .then()
+            .statusCode(200)
+            .extract()
+            .body()
+            .asString()
+
+        val createdPerson = objectMapper.readValue(content, PersonVO::class.java)
+
+        assertNotNull(createdPerson.id)
+        assertTrue(createdPerson.id > 0)
+        assertNotNull(createdPerson.firstName)
+        assertNotNull(createdPerson.lastName)
+        assertNotNull(createdPerson.address)
+        assertNotNull(createdPerson.gender)
+        assertEquals("Richard", createdPerson.firstName)
+        assertEquals("Richard", createdPerson.lastName)
+        assertEquals("New York City, New York, US", createdPerson.address)
+        assertEquals("Male", createdPerson.gender)
+    }
+
+    private fun mockPerson() {
+        person.firstName = "Richard"
+        person.lastName = "Richard"
+        person.address = "New York City, New York, US"
+        person.gender = "Male"
+    }
+
+}
