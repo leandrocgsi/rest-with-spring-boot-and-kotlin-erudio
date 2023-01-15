03ef49d9177ba43942f7b57b10fdec8bd949e538
leandrocgsi <leandrocgsi@gmail.com>
2023-01-15 15:20:19 -0300

    Connecting to MySQL with Spring Data, JPA and Hibernate

--- 08_ConnectingToMySQL/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/repository/PersonRepository.kt
+++ 08_ConnectingToMySQL/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/repository/PersonRepository.kt
@@ -2,7 +2,5 @@ package br.com.erudio.repository
 
 import br.com.erudio.model.Person
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 
-@Repository
 interface PersonRepository : JpaRepository<Person, Long?>
