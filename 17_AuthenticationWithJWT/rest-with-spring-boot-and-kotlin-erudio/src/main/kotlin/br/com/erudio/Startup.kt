90c8423a277443e02a5c8bfca31ee8c0f51fce7a
leandrocgsi <leandrocgsi@gmail.com>
2023-01-15 15:21:22 -0300

    Implementing Authentication With Spring Security and JWT

--- 17_AuthenticationWithJWT/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/Startup.kt
+++ 17_AuthenticationWithJWT/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/Startup.kt
@@ -1,12 +1,12 @@
 package br.com.erudio
 
-// import java.util.HashMap
+import java.util.HashMap
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-// import org.springframework.security.crypto.password.DelegatingPasswordEncoder
-// import org.springframework.security.crypto.password.PasswordEncoder
-// import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder
 
 
 @SpringBootApplication
@@ -15,13 +15,12 @@ class Startup
 fun main(args: Array<String>) {
 	runApplication<Startup>(*args)
 
-	/**
 	val encoders: MutableMap<String, PasswordEncoder> = HashMap()
-	encoders["pbkdf2"] = Pbkdf2PasswordEncoder()
+	val pbkdf2Encoder = Pbkdf2PasswordEncoder("", 8, 185000, Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256)
+	encoders["pbkdf2"] = pbkdf2Encoder
 	val passwordEncoder = DelegatingPasswordEncoder("pbkdf2", encoders)
-	passwordEncoder.setDefaultPasswordEncoderForMatches(Pbkdf2PasswordEncoder())
+	passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder)
 
 	val result = passwordEncoder.encode("foo-bar")
 	println("My hash $result")
-	*/
 }
