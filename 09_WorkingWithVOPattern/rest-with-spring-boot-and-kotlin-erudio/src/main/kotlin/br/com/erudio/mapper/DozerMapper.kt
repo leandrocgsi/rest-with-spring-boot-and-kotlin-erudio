7fca14b8b93eb421b596a98787bb81203dbe0d38
leandrocgsi <leandrocgsi@gmail.com>
2023-01-15 15:20:33 -0300

    Implementing VO pattern with Dozer

--- 09_WorkingWithVOPattern/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/mapper/DozerMapper.kt
+++ 09_WorkingWithVOPattern/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/mapper/DozerMapper.kt
@@ -3,9 +3,12 @@ package br.com.erudio.mapper
 import com.github.dozermapper.core.DozerBeanMapperBuilder
 import com.github.dozermapper.core.Mapper
 
+// import org.modelmapper.ModelMapper
+
 object DozerMapper {
 
     private val mapper: Mapper = DozerBeanMapperBuilder.buildDefault()
+    //private val mapper: ModelMapper = ModelMapper()
 
     fun <O,D> parseObject(origin: O, destination: Class<D>?): D {
         return mapper.map(origin, destination)
