153164a87b2c451d16a5c189bda04ff05b60e07b
leandrocgsi <leandrocgsi@gmail.com>
2023-12-19 22:35:35 -0300

    Working with Maven Archetypes

--- 25A_MavenArchetypes_OriginalProject/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/config/SecurityConfig.kt
+++ 25A_MavenArchetypes_OriginalProject/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/config/SecurityConfig.kt
@@ -1,6 +1,6 @@
 package br.com.erudio.config
 
-import br.com.erudio.security.jwt.JwtConfigurer
+import br.com.erudio.security.jwt.JwtTokenFilter
 import br.com.erudio.security.jwt.JwtTokenProvider
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
@@ -9,25 +9,28 @@ import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer
 import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @EnableWebSecurity
 @Configuration
 class SecurityConfig {
 
     @Autowired
-    lateinit var tokenProvider: JwtTokenProvider
+    private lateinit var tokenProvider: JwtTokenProvider
+
     @Bean
     fun passwordEncoder() : PasswordEncoder {
-        val encoders: MutableMap<String, PasswordEncoder> = HashMap()
-        val pbkdf2Encoder = Pbkdf2PasswordEncoder("", 8, 185000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256)
+        val encoders: MutableMap<String, PasswordEncoder> = HashMap<String, PasswordEncoder>()
+        val pbkdf2Encoder = Pbkdf2PasswordEncoder("", 8, 185000, Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256)
         encoders["pbkdf2"] = pbkdf2Encoder
         val passwordEncoder = DelegatingPasswordEncoder("pbkdf2", encoders)
         passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder)
@@ -41,29 +44,29 @@ class SecurityConfig {
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+
+        val customFilter = JwtTokenFilter(tokenProvider)
+
         return http
-            .httpBasic().disable()
-            .csrf { obj: CsrfConfigurer<HttpSecurity> -> obj.disable() }
-            .sessionManagement { session: SessionManagementConfigurer<HttpSecurity?> ->
-                session.sessionCreationPolicy(
-                    SessionCreationPolicy.STATELESS
-                )
+            .httpBasic{ basic: HttpBasicConfigurer<HttpSecurity> -> basic.disable()}
+            .csrf {csrf: CsrfConfigurer<HttpSecurity> -> csrf.disable()}
+            .addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .sessionManagement { session:
+                SessionManagementConfigurer<HttpSecurity?> ->
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             }
             .authorizeHttpRequests {
                 authorizeHttpRequests -> authorizeHttpRequests
                     .requestMatchers(
                         "/auth/signin",
                         "/auth/refresh/**",
-                        "/swagger-ui/**",
-                        "/v3/api-docs/**"
-                    ).permitAll()
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**")
+                    .permitAll()
                     .requestMatchers("/api/**").authenticated()
                     .requestMatchers("/users").denyAll()
             }
-                .cors()
-            .and()
-            .apply(JwtConfigurer(tokenProvider))
-            .and()
+            .cors {_: CorsConfigurer<HttpSecurity?>? ->}
             .build()
     }
 }
